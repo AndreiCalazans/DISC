83c19d4068360552c3f344febe8ca92ae9d27336
Andrei Calazans <andreixoc@hotmail.com>
2022-03-26 17:15:05 -0300

    more style fixes

--- app/reducers/reducers.js
+++ app/reducers/reducers.js
@@ -1,54 +1,49 @@
-
-
-
-
-
-export var questionIndexReducer = (state = 15 , action) => {
+export var questionIndexReducer = (state = 0, action) => {
   switch (action.type) {
-    case 'NEXT_QUESTION':
+    case "NEXT_QUESTION":
       return state + 1;
 
     default:
       return state;
-  };
+  }
 };
 
 var resultState = {
-  w: 30,
-  x: 49,
-  y: 38,
-  z: 33
+  w: 0,
+  x: 0,
+  y: 0,
+  z: 0
 };
 
 export var resultsReducer = (state = resultState, action) => {
   switch (action.type) {
-      case 'ADD_RESULT':
+    case "ADD_RESULT":
       let letter = action.letter;
       let value = Number(action.value);
       let previousValue = state[letter];
-      if (letter === 'w'){
+      if (letter === "w") {
         return {
           ...state,
           w: previousValue + value
-        }
-      } else if (letter === 'x'){
+        };
+      } else if (letter === "x") {
         return {
           ...state,
           x: previousValue + value
-        }
-      } else if (letter === 'y'){
+        };
+      } else if (letter === "y") {
         return {
           ...state,
           y: previousValue + value
-        }
-      } else if (letter === 'z'){
+        };
+      } else if (letter === "z") {
         return {
           ...state,
           z: previousValue + value
-        }
+        };
       }
 
     default:
       return state;
   }
-}
+};
